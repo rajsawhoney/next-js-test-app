58c9c6e19c970bfbdf596471a37ee47ff95e858f
Razz Sawhoney <rajsahani1819@gmail.com>
2023-08-03 14:07:47 +0545

    feat:initial project setup using tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+export default function Player() {
+  return (
+    <div className="flex flex-col w-full h-screen bg-red-500 items-center justify-center">
+      <title>Player Page</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        property="og:url"
+        content="https://d3puv92mi2kxt8.cloudfront.net/VeelEnterpriseBanner.mp4"
+      />
+      <meta property="og:type" content="video" />
+      <meta property="og:title" content="My Awesome Video Test" />
+      <meta
+        property="og:description"
+        content="My Awesome Video Test Description"
+      />
+      <meta
+        property="og:image"
+        content="https://img.freepik.com/premium-psd/new-modern-glow-effect-video-game-review-youtube-channel-thumbnail-web-banner-premium-psd-templa_623685-102.jpg"
+      />
+      <meta
+        property="og:image:secure_url"
+        content="https://img.freepik.com/premium-psd/new-modern-glow-effect-video-game-review-youtube-channel-thumbnail-web-banner-premium-psd-templa_623685-102.jpg"
+      />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta
+        property="og:video"
+        content="https://d3puv92mi2kxt8.cloudfront.net/VeelEnterpriseBanner.mp4"
+      />
+      <meta
+        property="og:video:secure_url"
+        content="https://d3puv92mi2kxt8.cloudfront.net/VeelEnterpriseBanner.mp4"
+      />
+      <meta property="og:video:type" content="video/mp4" />
+      <h1>This is a Player Screen</h1>
+    </div>
+  );
+}
