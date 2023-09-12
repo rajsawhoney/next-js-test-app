b407fe7314d9794b4a047ea7509d5e7720bf1f47
Razz Sawhoney <rajsahani1819@gmail.com>
2023-09-12 08:49:20 +0545

    testing app deeplink

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,12 @@ export default function Home() {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <div className="flex flex-col w-screen bg-blue-400 h-screen items-center justify-center">
         <h1>This is a Home Screen</h1>
+        <a
+          className="px-4 py-4 text-blue-600 text-2xl"
+          href="veelapp-devclient://"
+        >
+          Open App
+        </a>
       </div>
     </Layout>
   );
